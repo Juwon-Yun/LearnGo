d22ac2f5c2041a1f9635bf4f6e2c94e19955be79
Juwon-Yun <eooll2@daum.net>
2022-01-30 16:58:01 +0900

    if, switch, pointers

---
+++ src/main_if_switch.go
@@ -0,0 +1,34 @@
+package mamain_if_switch
+
+import "fmt"
+
+func canIDrink(age int) bool {
+
+	// koreanAge := age + 2
+
+	//variable expression go의 변수 표현식
+	// if koreanAge := age - 2; koreanAge < 20 {
+
+	// 조건을 체크하기전에 variable( 변수 )를 만들 수 있다
+	// if문에서만 사용하는 변수를 말한다
+	if koreanAge := age - 2; koreanAge < 20 {
+		return false
+	}
+	return true
+}
+
+func switchCanIDring(age int) bool {
+	// switch koreanAge := age - 2; koreanAge {
+	switch koreanAge := age - 2; {
+	case koreanAge < 18:
+		return false
+	case koreanAge > 50:
+		return false
+	}
+	return true
+}
+
+func mamain_if_switch() {
+	fmt.Println(canIDrink(22))
+	fmt.Println(switchCanIDring(19))
+}
