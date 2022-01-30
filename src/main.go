cba372a01ae98b6491734928847342960d8723a1
Juwon-Yun <eooll2@daum.net>
2022-01-30 17:35:23 +0900

    Array, Slice, Map, Struct

--- src/main.go
+++ src/main.go
@@ -2,51 +2,6 @@ package main
 
 import "fmt"
 
-// 메모리 주소를 보는법과
-// 어떤것이 메모리 주소에 접근하도록하는 방법
-
 func main() {
-	// low level programming
-	// go의 특징 중 하나 :
-	// hight level programming 기법으로
-	// low level programming이 가능하게 해줌
-	a := 2
-	// b := a
-	b := &a
-	// value 복사임
-	a = 10
-	// javascript의 깊은복사와 얕은복사를 의미하나?
-	// fmt.Println(a, b)
-
-	// 0xc0000140d0 0xc0000140d8 메모리 주소를 볼 수 있다
-	fmt.Println(&a, &b)
-
-	// 0xc0000b8010 0xc0000b8010 메모리 주소를 복사함
-	fmt.Println(&a, b)
-
-	// 여기서 b 는 a의 메모리 주소에 연결되어있어 a의 값을 참조한다.
-
-	// 살펴보거나 훑어보는 느낌의 *
-	fmt.Println(*b)
-
-	// pointer를 사용하는 이유는 값 복사 대신에
-	// 메모리에 저장된 Object( 객체 )를 서로 똑같이 가지고 싶도록
-	// 원할 때 사용한다 (=> javascript의 [...Object] )
-
-	// 결과적으로 이러한 메모리 주소 복사 방법의 코드가
-	// 프로그램을 더 빠르게 동작할 수 있게하는 기반을 만들어준다.
-	// 즉 계속 value 복사를 하게되면 느려짐 (=> 다른 메모리 주소에 같은 값 복사)
-
-	// 깊은 복사의 상태에서 b를 가지고 a의 값 변경이 가능하다.
-	*b = 20
-	fmt.Println(a)
-
-	// 이것들이 바로 go에서의 low level programming 기법이다.
-
-	// 결론 & => address
-	// * => 살펴보는것 ( see through )
-	// *를 사용해서 주소를 살펴보면 주소에 담긴 값을 볼 수 있다.
-	// 또한 주소를 갖고있는 변수는 주소에 담긴 값을 변경할 수 있다.
-
-	// 이게 Go에서 a를 poiner b를 사용해 값을 변경할 수 있는 이유
+	fmt.Println("init")
 }
---
+++ src/main_Array_Slices_Map_Struct.go
@@ -0,0 +1,76 @@
+package main_Array_Slices_Map_Struct
+
+import "fmt"
+
+func main_Array_Slice() {
+	// names := [5]string{"juwon1", "juwon2", "juwon3"}
+	// 길이제한 없애기
+	names := []string{"juwon1", "juwon2", "juwon3"}
+
+	// go 에서는 이런 식으로는 goroutine을 발생시킴
+	// names[3] = "alalla"
+	// names[4] = "alalla"
+
+	// 크리와 범위를 정했기 때문에 out of bounds 에러 발생함
+	// names[5] = "alalla"
+
+	// 길이제한을 없앤 후에 요소 추가는 append() 한다
+	// unshift도 있나?
+
+	// 새로운 slice를 return 한다.
+	names = append(names, "juwon5")
+
+	// 없다
+	// names = unshift(names, "unshift juwon")
+
+	fmt.Println(names)
+
+}
+
+// struct => object와 class가 합쳐진 개녕
+
+func main_Map() {
+	// charactor 형태의 map
+	// 		 map[key type]value type{key:value}
+	juwon := map[string]string{"name": "juwon", "age": "12"}
+	fmt.Println(juwon)
+
+	// map도 for range로 loop 동작이 가능하다
+	for _, value := range juwon {
+		fmt.Println(value)
+	}
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
+func main_Array_Slices_Map_Struct() {
+	// 이와 같은 것들을 선언하고싶으면?
+	// {
+	// 	"name" : "juwon",
+	// 	"age" : 30,
+	// 	"favFood" : ["string"]
+	// }
+	favFood := []string{"kimchi", "ramen"}
+	favFood2 := []string{"cake", "bread"}
+
+	// struct를 만드는 방법 2개
+	juwon := person{"juwon", 30, favFood}
+	// key 값을 하나라도 명시했으면 다른 요소들도 필수로 명시해줘야한다.
+	juwon2 := person{name: "juwon2", age: 55, favFood: favFood2}
+	fmt.Println(juwon)
+	fmt.Println(juwon2)
+
+	// go에서는 생성자 메소드가 없다
+	// javascript constructor
+	// python __init__
+	// java new Name();
+
+	// struct는 없다
+	// 그래서 스스로 생성자를 실행 시켜야함
+
+	// 아직 설명하지 않은것들 channel, go routine
+}
